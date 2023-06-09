3034fa1b7d28a9e986e27af256827c7e98f07477
Maria K Dominic <46400263+mariakdominic@users.noreply.github.com>
2023-06-09 19:07:26 +0100

    Add files via upload

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -71,40 +71,4 @@ const Home = () => {
   );
 };
 
-// class Home extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <SearchBar onTermChange={this.props.actions.requestGifs} />
-
-//                 <GifList gifs={ this.props.gifs }
-//                          onGifSelect={ selectedGif => this.props.actions.openModal({selectedGif}) }
-//                          onFavoriteSelect={ selectedGif => this.props.actions.favoriteGif({selectedGif}) }
-//                          onFavoriteDeselect={ selectedGif => this.props.actions.unfavoriteGif({selectedGif}) }
-//                         //  isAuthenticated={ this.props.authenticated }
-//                           />
-
-//                 <GifModal modalIsOpen={ this.props.modalIsOpen }
-//                           selectedGif={ this.props.selectedGif }
-//                           onRequestClose={ () => this.props.actions.closeModal() } />
-//             </div>
-//         );
-//     }
-// }
-
-// function mapStateToProps(state) {
-//     return {
-//         // authenticated: state.auth.authenticated,
-//         gifs: state.gifs.data,
-//         modalIsOpen: state.modal.modalIsOpen,
-//         selectedGif: state.modal.selectedGif
-//     };
-// }
-
-// function mapDispatchToProps(dispatch) {
-//     return {
-//         actions: bindActionCreators(Actions, dispatch)
-//     };
-// }
-
 export default connect()(Home);
